3bcc9dea21a4c984b1a35e5c474e6d21a29b2357
Christophe VG <xtof@pumpkin.local>
2010-11-17 22:09:35 +0100

    added Makefile to create a clean build before deploy added ADL submodule added local antlr3 module

---
+++ Makefile
@@ -0,0 +1,18 @@
+BUILD_DIR=build
+SRCS=*.py *.yaml templates images css
+LIBS=lib/ADL/build/adl lib/antlr3
+  
+all: ${BUILD_DIR}
+
+${BUILD_DIR}: clean ${SRCS} ${LIBS}
+	@echo "** rebuilding tree"
+	@mkdir ${BUILD_DIR}
+	@cp -r ${SRCS} ${BUILD_DIR}
+	@cp -r ${LIBS} ${BUILD_DIR}
+
+lib/ADL/build/adl:
+	@(cd lib/ADL; make all-python)
+
+clean:
+	@rm -rf ${BUILD_DIR}
+	@rm -f *.pyc
--- SubmitDiagram.py
+++ SubmitDiagram.py
@@ -9,24 +9,30 @@ from Account import get_account_for_user
 from Home import Home
 from Diagram import Diagram
 
+from adl import adl
+
 class SubmitDiagram(webapp.RequestHandler):
   def get(self):
     # nothing to get here ...
     self.redirect("/");
+
+  def get_source(self):
+    model = adl.parse(self.request.get("src"))
+    return "\n".join(["%s" % (s) for s in model])
 
   def post(self):
     user    = users.get_current_user()
     account = get_account_for_user(user)
 
     # if the user is logged on, but we find no account, provision him first
-    if user <> None and account == None:
+    if user != None and account == None:
       Home.show( account, self, "A complete account is needed to submit." );
       return
 
     diagram = {
       'author'      : account,
       'id'          : self.request.get("id"),
-      'source'      : self.request.get("src"),
+      'source'      : self.get_source(),
       'name'        : self.request.get("name"),
       'description' : self.request.get("descr"),
       'width'       : int(self.request.get("width", default_value="300")),
@@ -44,7 +50,7 @@ class SubmitDiagram(webapp.RequestHandler):
       
   def save(self, diagram):
     prev_diagram = None
-    if diagram['id'] <> None and diagram['id'] <> "":
+    if diagram['id'] != None and diagram['id'] != "":
       prev_diagram = Diagram.get_by_key_name(diagram['id']) 
     
     if prev_diagram == None:
